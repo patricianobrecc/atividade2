ceffabc66dcb1f223786c3b67c6d34e660776433
patricianobrecc <71797649+patricianobrecc@users.noreply.github.com>
2020-09-26 17:12:10 -0300

    Update Atividade 2 - Exercicio 5.cpp

--- Atividade 2 - Exercicio 5.cpp
+++ Atividade 2 - Exercicio 5.cpp
@@ -3,14 +3,17 @@
   int main(){  
   int numero[100];  
   int razao, n1;   
-  printf ("Digite um valor inicial o qual iniciara nossa sequencia da PA: ");  
-  scanf ("%d" , &n1);  
-  printf ("Digite um numero como razao para nossa sequencia: ");  
-  scanf ("%d" , &razao);  
-  for (int i=0; i<100; i++){  
-   printf ("%d" , n1);  
-   printf ("  ");
-   n1=n1+razao;  
+  	
+		printf ("Digite um valor inicial o qual iniciara nossa sequencia da PA: ");  
+  		scanf ("%d" , &n1);  
+  
+  		printf ("Digite um numero como razao para nossa sequencia: ");  
+  		scanf ("%d" , &razao);  
+  
+  			for (int i=0; i<100; i++){  
+   			printf ("%d" , n1);  
+   			printf ("  ");
+   			n1=n1+razao;  
   }  
   return 0;  
   } 
