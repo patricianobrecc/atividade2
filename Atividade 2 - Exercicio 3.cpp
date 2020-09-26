968532123793d5992180bf6e82b6371fa5f26b55
patricianobrecc <71797649+patricianobrecc@users.noreply.github.com>
2020-09-26 17:10:05 -0300

    Update Atividade 2 - Exercicio 3.cpp

--- Atividade 2 - Exercicio 3.cpp
+++ Atividade 2 - Exercicio 3.cpp
@@ -3,12 +3,12 @@
 int main()
 {
 int fat, n;
-printf("Insira um valor para o qual deseja calcular seu fatorial: ");
-scanf("%d", &n);
+	printf("Insira um valor para o qual deseja calcular seu fatorial: ");
+	scanf("%d", &n);
  
-for(fat = 1; n > 1; n = n - 1)
-fat = fat * n;
+		for(fat = 1; n > 1; n = n - 1)
+		fat = fat * n;
  
-printf("\nFatorial calculado: %d", fat);
-return 0;
+			printf("\nFatorial calculado: %d", fat);
+	return 0;
 }
